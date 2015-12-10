8e223b543fcc1fb7144f8f49b0c4969f4949e5c1
Stefan Schulz <schulzs@students.uni-marburg.de>
2015-12-10 13:10:39 +0900

    Made the script more verbose

--- purge_rhizome.sh
+++ purge_rhizome.sh
@@ -5,9 +5,13 @@ RHIZOME_STORE_PATH=$(./servald config paths | grep RHIZOME_STORE_PATH | awk 'BEG
 echo "Stopping servald..."
 ./servald stop
 
-echo "Trying to purge $RHIZOME_STORE_PATH..."
-read -p "Continue (y/n)? " choice
+echo
+
+read -p "Trying to purge $RHIZOME_STORE_PATH. Continue (y/n)? " choice
 case "$choice" in 
-  y|Y ) rm -rf "$RHIZOME_STORE_PATH";;
+  y|Y ) 
+    echo "Purging $RHIZOME_STORE_PATH..."
+    rm -rf "$RHIZOME_STORE_PATH"
+  ;;
   * ) exit 1;;
 esac
